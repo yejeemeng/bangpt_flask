74d5c3c428a81cc1e69750c6046d21a3ba7f4d8f
김예지 <yejeejudy@naver.com>
2023-05-29 23:49:16 +0900

    diary 콜렉션 추가

---
+++ diary.py
@@ -0,0 +1,31 @@
+from datetime import datetime, date
+from flask import *
+from pymongo import MongoClient
+from bson.json_util import dumps
+
+#--------------------------------------------------------------------------------------------------------------------#
+client = MongoClient("mongodb://localhost:27017/hyeyeon?retryWrites=true&w=majority")
+db = client.capstone_design
+Diary = db['diary'] # 다이어리 콜렉션
+Diary_bp = Blueprint('diary', __name__)
+#--------------------------------------------------------------------------------------------------------------------#
+
+@Diary_bp.route('/new', methods=['POST']) # 운동 다이어리 추가
+def new_diary():
+
+    diary_id = request.form['diaryID']
+    diary_date = request.form['diaryDate']
+    diary_num = Diary.count_documents({"diary_id": diary_id, "diary_date": diary_date}) +1
+    diary_memo = request.form['diaryMemo']
+
+    diary = {
+        'diary_id': diary_id,
+        'diary_date': diary_date,
+        'diary_num': diary_num,
+        'diary_memo': diary_memo
+    }
+    # MongoDB에 추가
+    Diary.insert_one(diary)
+    response = {'success': True}
+
+    return jsonify(response)

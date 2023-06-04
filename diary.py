488e2783839432a58271c50429f2e0af5487a8f1
Eunbi An <ibnuena@naver.com>
2023-06-04 15:28:12 +0900

    calendar dot

--- diary.py
+++ diary.py
@@ -4,7 +4,6 @@ from pymongo import MongoClient
 from bson.json_util import dumps
 from user_badge import update_attendance
 
-##
 #--------------------------------------------------------------------------------------------------------------------#
 client = MongoClient("mongodb://localhost:27017/hyeyeon?retryWrites=true&w=majority")
 db = client.capstone_design
@@ -29,10 +28,28 @@ def new_diary():
         'diary_num': diary_num,
         'diary_memo': diary_memo
     }
-    
     # MongoDB에 추가
     Diary.insert_one(diary)
     update_attendance(user_id)
     response = {'success': True}
 
     return jsonify(response)
+
+@Diary_bp.route('/get/<diary_id>/<diary_date>', methods=['GET']) # 해당일의 운동 다이어리 읽어들이기
+def get_diary(diary_id, diary_date):
+
+    cursor = Diary.find({"diary_id": diary_id, "diary_date": diary_date})
+    print(diary_id)
+    print(diary_date)
+
+    memos = list(cursor)
+    return dumps(memos)
+
+@Diary_bp.route('/get/<diary_id>/<diary_year>/<diary_month>', methods=['GET']) # 해당 월의 운동한 날짜 읽어들이기
+def get_date(diary_id, diary_year, diary_month):
+
+    string = diary_year + "-" + f"{int(diary_month):02}"
+    cursor = Diary.find({"diary_id": diary_id , "diary_num": 1, "diary_date": {"$regex" : string}})
+
+    exercise_dates = list(cursor) #해당 월에 운동한 날들에 해당하는 도큐멘트
+    return dumps(exercise_dates)
